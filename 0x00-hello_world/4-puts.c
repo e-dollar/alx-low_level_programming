d05da15c816a28b1aa1ba9a48f8d9cd1d6656ec3
e-dollar <chukwudaluemmanuel7@gmail.com>
2022-09-08 11:30:37 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,11 @@
-#!/bin/bash
 #include <stdio.h>
 
 int main()
 {
 
-  char str[] = "programming is like building a multilingual puzzle";
+    puts("\"Programming is like buildint a multilingual puzzle");
 
-  puts(str);
-
-  return(0);
+    return(0);
+	 
 
 }
