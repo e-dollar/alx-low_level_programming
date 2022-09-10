5b56a8069958e603bf9dcd1c72af755c762ae46e
e-dollar <chukwudaluemmanuel7@gmail.com>
2022-09-10 01:29:35 -0700

    print all single digits(rescriptive)

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+*main - prints all single digits in base 10
+*Description: prints numbers from 0 - 9
+*Return: always return 0 (Success)
+*/
+
+int main(void)
+{
+int i;
+i = 48
+while (i < 58)
+{
+putchar(i);
+i++;
+}
+putchar(10);
+return (0);
+}
