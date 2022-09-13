deb5581475eb478a9f96e36c37ef857d54c8c2f4
e-dollar <chukwudaluemmanuel7@gmail.com>
2022-09-13 11:54:56 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+*main - print alphabets
+*descrption: print all the letters of the alphabet in lower case
+*return: always return 0
+*/
+
+
+void print_alphabet(void)
+{
+int i;
+i = 97;
+while (1 < 123)
+{
+_putchar(i);
+}
+i++;
+}
