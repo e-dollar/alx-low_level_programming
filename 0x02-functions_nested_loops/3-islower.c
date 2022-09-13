ab3e39e7d46089ea148ea09fc87684348fe49827
e-dollar <chukwudaluemmanuel7@gmail.com>
2022-09-13 13:55:28 -0700

    print 1 if it is lower case

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks for alphabetic character
+ * @c: c is an ascii character
+ *
+ * Return: 1 (if letter)
+ */
+
+int islower(int c)
+{
+if (c >='a' && c <= 'z')
+{
+return (1);
+else
+{
+return (0);
+}
+}
+}
