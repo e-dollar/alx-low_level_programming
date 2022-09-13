ca041190f08de186255b036999990849aea95d55
e-dollar <chukwudaluemmanuel7@gmail.com>
2022-09-13 13:13:32 -0700

    alphabet times 10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+*print_alphabet_x10 - prints alphabets 10 times
+*return (void)
+*/
+
+
+void print_alphabet_x10(void)
+{
+int i = 0;
+char l = 'a'
+while (i < 10)
+{
+while (l <= 'z')
+{
+_putchar(l);
+l++;
+}
+_putchar('\n');
+i++
+}
+}
