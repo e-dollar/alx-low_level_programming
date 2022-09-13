17cef5272f7fe980099fc4112f68fed1bbb03b86
e-dollar <chukwudaluemmanuel7@gmail.com>
2022-09-13 07:32:12 -0700

    print _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+*main - prints _putchar
+*description: a code that prints _putchar
+*Return: return 0 (Success)
+*/
+
+int main(void)
+{
+printf("_putchar");
+return (0);
+}
