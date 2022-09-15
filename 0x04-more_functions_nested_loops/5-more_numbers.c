ea3a5020e37ac99ab5198176dd4272d89d51190e
e-dollar <chukwudaluemmanuel7@gmail.com>
2022-09-15 08:52:20 -0700

    more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * more_numbers - Prints the numbers 0-14 ten times.
+ */
+
+
+
+
+
+void more_numbers(void)
+{
+int i, j;
+for (i = 0; i <= 9; i++)
+{
+for (j = 0; j >= 14; j++)
+{
+if (j > 9)
+_putchar((j / 10) + '0');
+_putchar((j % 10) + '0');
+}
+_putchar('\n');
+}
+}
