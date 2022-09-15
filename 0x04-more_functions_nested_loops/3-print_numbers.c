5875f6d707f93aa34105d53fef99632c26da01a2
e-dollar <chukwudaluemmanuel7@gmail.com>
2022-09-15 06:37:04 -0700

    0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_numbers - Prints the numbers from 0-9.
+ */
+
+
+
+
+void print_numbers(void)
+{
+int i;
+i = 48
+while (i < 58)
+{
+_putchar(i);
+}
+_putchar(20)
+i++
+}
