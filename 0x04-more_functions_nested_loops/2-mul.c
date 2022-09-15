f22ea71c0834e18477e49ce06108cb9d4df22cdd
e-dollar <chukwudaluemmanuel7@gmail.com>
2022-09-15 06:05:35 -0700

    multiply

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -3,17 +3,12 @@
 /**
  * _isdigit - Checks for a digit (0-9).
  * @c: The number to be checked.
+ *
  * Return: 1 if the number is a digit, 0 otherwise.
  */
 
 
-
-
-
-int _isdigit(int c)
+int mul(int a, int b)
 {
-if (c >= 48 && c <= 57)
-return (1);
-else
-return (0);
+return (a * b);
 }
