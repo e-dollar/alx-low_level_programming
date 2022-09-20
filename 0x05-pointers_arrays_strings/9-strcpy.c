628a30b638437e557e64e66a8cecfc58181cef1d
e-dollar <chukwudaluemmanuel7@gmail.com>
2022-09-20 14:41:05 -0700

    string copy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -11,7 +11,7 @@
  * Return: A pointer to the destination string @dest.
  */
 
-char *_strcpy(char *dest, char *src)
+char *_strcpy(char *dest, const char *src)
 {
 int index = 0;
 while (src[index])
@@ -19,6 +19,5 @@ while (src[index])
 dest[index] = src[index];
 index++;
 }
-dest(index) = '\0';
 return (dest);
 }
