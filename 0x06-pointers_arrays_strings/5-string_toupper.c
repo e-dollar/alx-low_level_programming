0be556cde3c804295fb19c1d155a152ac960e1f8
e-dollar <chukwudaluemmanuel7@gmail.com>
2022-09-21 16:28:59 -0700

    change lower case to upper case:

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -2,9 +2,11 @@
 
 
 /**
- * string_toupper - changes all lowercase of string to uppercase
- * @s: string array
- * Return: string array
+ * string_toupper - Changes all lowercase letters
+ *                  of a string to uppercase.
+ * @str: The string to be changed.
+ *
+ * Return: A pointer to the changed string.
  */
 
 char *string_toupper(char *str)
