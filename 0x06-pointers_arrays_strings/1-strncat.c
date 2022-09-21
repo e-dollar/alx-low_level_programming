9d7d9cd2c320cfe42d098399863d767a2277e8ae
e-dollar <chukwudaluemmanuel7@gmail.com>
2022-09-21 05:58:31 -0700

    concatenate n of string

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+
+
+char *_strncat(char *dest, char *src, int n);
+{
+int i;
+int len;
+len = 0;
+while (dest[len]!= "\0")
+{
+len++;
+}
+for (i = 0; i < n && src[i] != "\0" i++)
+dest[len + i] = src[i];
+destt[len + i] = "\0";
+return (dest);
+}
