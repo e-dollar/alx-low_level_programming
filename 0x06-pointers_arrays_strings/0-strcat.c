657424d07044da4c23e47b2b9f7689979f7eb8d7
e-dollar <chukwudaluemmanuel7@gmail.com>
2022-09-21 08:48:04 -0700

    copy n number of string

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -14,12 +14,12 @@ char *_strcat(char *dest, char *src)
 {
 int i;
 int n = 0;
-while (dest[n] != "\0")
+while (dest[n] != '\0')
 {
 n++;
 }
-for (i = 0; i < n && src[i] != "\0"; i++)
+for (i = 0; i < n && src[i] != '\0'; i++)
 dest[n + i] = src[i];
-dest[n + i] = "\0";
+dest[n + i] = '\0';
 return (dest);
 }
--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _strncat - concatenates two strings
+ * _strncpy - function that copies a string
  * @dest: destination char
  * @src: source char
  * @n: number of bytes from src
@@ -10,17 +10,12 @@
 
 
 
-char *_strncat(char *dest, char *src, int n)
+char *_strncpy(char *dest, char *src, int n)
 {
 int i;
-int len;
-len = 0;
-while (dest[len] != '\0')
-{
-len++;
-}
 for (i = 0; i < n && src[i] != '\0'; i++)
-dest[len + i] = src[i];
-dest[len + i] = '\0';
+dest[i] = src[i];
+for ( ; i < n; i++)
+dest[i] = '\0';
 return (dest);
 }
