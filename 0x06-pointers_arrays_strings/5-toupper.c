1e10bbda4c55a6ceb0a4200a78a10daf062b433a
e-dollar <chukwudaluemmanuel7@gmail.com>
2022-09-21 15:54:58 -0700

    change lower case to upper case

---
+++ 0x06-pointers_arrays_strings/5-toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+
+/**
+ * string_toupper - changes all lowercase of string to uppercase
+ * @s: string array
+ * Return: string array
+ */
+
+char *string_toupper(char *str)
+{
+int i;
+for (i = 0; s[i] != '\0'; i++)
+{
+if (i >= 'a' && i <= 'z')
+{
+s[i] = s[i] - 32;
+}
+}
+}
