76a6f6c79108fb6b98e310b1b80d1a9b7a597b12
e-dollar <chukwudaluemmanuel7@gmail.com>
2022-09-22 09:52:32 -0700

    leet

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+
+
+
+
+
+char *leet(char *s)
+{
+int i = 0;
+while (s[i] != '\0')
+{
+if (s[i] == 'a' || s[i] == 'A')
+s[i] = '4';
+if (s[i] == 'e' || s[i] == 'E')
+s[i] = '3';
+if (s[i] == 'o' || s[i] == 'O')
+s[i] = '0';
+if (s[i] == 't' || s[i] == 'T')
+s[i] = '7';
+if (s[i] == 'l' || s[i] == 'L')
+s[i] = '1';
+}
+i++;
+return (s);
+}
