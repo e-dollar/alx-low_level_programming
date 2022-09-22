ebce081b6c8bd48aefa4ca97446dca2c2aad703a
e-dollar <chukwudaluemmanuel7@gmail.com>
2022-09-22 10:10:54 -0700

    leet

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -13,15 +13,15 @@ char *leet(char *s)
 {
 int indx1 = 0, indx2;
 char leet[8] = {'O', 'L', '?', 'E', 'A', '?', '?', 'T'};
-while (str[indx1])
+while (s[indx1])
 {
 for (indx2 = 0; indx2 <= 7; indx2++)
 {
-if (str[indx1] == leet[indx2] ||
-str[indx1] - 32 == leet[indx2])
-str[indx1] = indx2 + '0';
+if (s[indx1] == leet[indx2] ||
+s[indx1] - 32 == leet[indx2])
+s[indx1] = indx2 + '0';
 }
 indx1++;
 }
-return (str);
+return (s);
 }
