04daf355ab585ee5a029263f97918b67ffac6a7c
e-dollar <chukwudaluemmanuel7@gmail.com>
2022-09-26 12:49:10 -0700

    search string

--- 0x07-pointers_arrays_strings/0x07-pointers_arrays_strings.c
+++ 0x07-pointers_arrays_strings/0x07-pointers_arrays_strings.c
@@ -11,12 +11,24 @@
 
 
 char *_strchr(char *s, char c)
+
 {
-int index;
-for (index = 0; s[index] >= '\0'; index++)
-{
-if (s[index] == c)
-return (s + index);
-}
-return ('\0');
+
+	int index;
+
+
+
+	for (index = 0; s[index] >= '\0'; index++)
+
+	{
+
+		if (s[index] == c)
+
+			return (s + index);
+
+	}
+
+
+
+	return ('\0');
 }
