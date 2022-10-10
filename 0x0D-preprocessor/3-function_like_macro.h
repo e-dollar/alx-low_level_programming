ca8dfbb9071f2ed0db6adfd8642e94959cc8ef51
e-dollar <chukwudaluemmanuel7@gmail.com>
2022-10-10 15:09:27 -0700

    print absolute number

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,3 +1,7 @@
-#ifndef ABS
-#define ABS(x) (x)
-#endif
+#ifndef FUNCTION_LIKE_MACRO_H
+
+#define FUNCTION_LIKE_MACRO_H
+
+#define ABS(num) ((num) > (-1) ? (num) : (num * -1))
+
+#endif /* !FUNCTION_LIKE_MACRO_H */
