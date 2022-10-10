7525f3efa611a0c149b54a1cdc483344e177de4d
e-dollar <chukwudaluemmanuel7@gmail.com>
2022-10-10 15:21:22 -0700

    print absolute number

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -2,6 +2,18 @@
 
 #define FUNCTION_LIKE_MACRO_H
 
-#define ABS(x) ((x) > (-1) ? (x) : (x * -1))
 
-#endif /* !FUNCTION_LIKE_MACRO_H */
+
+/*
+ * File: 3-function_like_macro.h
+ * Desc: Header file that defines a function-like macro ABS(x)
+ *       that computes the absolute value of a number x.
+ */
+
+
+
+#define ABS(x) ((x) < 0 ? (-x) : (x))
+
+
+
+#endif
